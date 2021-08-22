6014265b05bfa758664f1c46aa09d37b29105246
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-08-22 16:17:21 -0300

    [ADD] Adiciona logica do header para naveegaçao sem rotas

--- vinigofr-portfolio/src/Components/Header.jsx
+++ vinigofr-portfolio/src/Components/Header.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import '../general-style.css';
 
-export default function Header() {
+const headerButtons = [{
+  text: 'Projetos',
+  value: 'projects',
+},{
+  text: 'Contato',
+  value: 'contact',
+},
+{
+  text: 'GitHub',
+  value: 'github',
+},
+{
+  text: 'LinkedIn',
+  value: 'linkedin',
+}
+]
+
+export default function Header(props) {
+  const { setPage } = props;
+
+  function choosePage(page) {
+    setPage(page.id);
+  };
+
   return (
-    <div>
-      <header>
-        
+      <header className="main-header">
+        {headerButtons.map(({text, value}) => (
+          <p 
+          className="main-header" 
+          id={ value }
+          onClick={ (e) => choosePage(e.target) }
+          >
+            {text}
+          </p>
+        ))}
       </header>
-    </div>
-  )
+  );
 }
--- vinigofr-portfolio/src/Pages/Home.jsx
+++ vinigofr-portfolio/src/Pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Header from '../Components/Header';
 function Home() {
+  const [page, setPage] = useState('home'); 
   return (
     <div>
-      Página inicial React
+      {console.log(page)}
+      <Header setPage={ setPage } />
+      { page }
     </div>
   )
 }
