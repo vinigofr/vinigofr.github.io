188b11b793d119331fc10cf3c0fe33f2a69db437
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-10 16:50:28 -0300

    [REM] Remove regras de negocio relacionada a github e linkedin

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,8 +11,6 @@ function Home() {
       <Header setPage={ setPage } currPage={ page } />
       { page === 'home' ? <Presentation /> : null }
       { page === 'projects' ? <Projects /> : null }
-      { page === 'github' ? 'github' : null }
-      { page === 'linkedin' ? 'linkedin' : null }
       { page === 'contact' ? 'contato' : null }
     </div>
   )
