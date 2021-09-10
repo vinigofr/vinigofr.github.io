b90ddac213af26ea31646519512cc0411a28e166
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-10 19:19:43 -0300

    [ADD] Lib para email

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Contact() {
   return (
     <div>
+      <div>
+        forms para email
+      </div>
       <email>freitas.viniciuspk@outlook.com.br</email>
+      <br />
+      <phone>+55 99 9 8111 0785</phone>
     </div>
   )
 }
