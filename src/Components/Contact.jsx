54d2101c3274b2d506c8967459d7466e268ba32a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-10 23:10:37 -0300

    [ADD] Email service

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,16 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
+import { init, send } from "emailjs-com";
+init("user_3bguTCeUpXJpsAeCpvezS");
 
 function Contact() {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_r8dnbhc',
+      'template_h3b3zn1',
+      toSend,
+      'user_3bguTCeUpXJpsAeCpvezS'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
   return (
-    <div>
-      <div>
-        forms para email
-      </div>
-      <email>freitas.viniciuspk@outlook.com.br</email>
-      <br />
-      <phone>+55 99 9 8111 0785</phone>
-    </div>
-  )
+    <>
+      <h1>Que tal conversarmos um pouco?</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="from_name"
+          placeholder="from name"
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="to_name"
+          placeholder="to name"
+          value={toSend.to_name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="Your message"
+          value={toSend.message}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="reply_to"
+          placeholder="Your email"
+          value={toSend.reply_to}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
