f95fb431f011da5d025723e8f125e556fd73f6da
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-18 00:07:28 -0300

    Adiciona feature place-holder

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,25 +4,24 @@ init("user_3bguTCeUpXJpsAeCpvezS");
 
 function Contact() {
   const [toSend, setToSend] = useState({
-    from_name: '',
-    to_name: '',
-    message: '',
-    reply_to: '',
+    from_name: "",
+    message: "",
+    reply_to: "",
   });
 
   const onSubmit = (e) => {
     e.preventDefault();
     send(
-      'service_r8dnbhc',
-      'template_h3b3zn1',
+      "service_r8dnbhc",
+      "template_h3b3zn1",
       toSend,
-      'user_3bguTCeUpXJpsAeCpvezS'
+      "user_3bguTCeUpXJpsAeCpvezS"
     )
       .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
+        console.log("SUCCESS!", response.status, response.text);
       })
       .catch((err) => {
-        console.log('FAILED...', err);
+        console.log("FAILED...", err);
       });
   };
 
@@ -30,40 +29,48 @@ function Contact() {
     setToSend({ ...toSend, [e.target.name]: e.target.value });
   };
   return (
-    <>
+    <div>
       <h1>Que tal conversarmos um pouco?</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="from_name"
-          placeholder="from name"
-          value={toSend.from_name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="to_name"
-          placeholder="to name"
-          value={toSend.to_name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="Your message"
-          value={toSend.message}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="reply_to"
-          placeholder="Your email"
-          value={toSend.reply_to}
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
+      <form onSubmit={onSubmit} className="contact-forms">
+        <label className="input-label">
+          Como você se chama?
+          <input
+            className="contact-input"
+            type="text"
+            name="from_name"
+            placeholder="Digite seu nome e sobrenome"
+            value={toSend.from_name}
+            onChange={handleChange}
+          />
+        </label>
+        <label className="input-label">
+          E agora, seu email para retorno
+          <input
+            className="contact-input"
+            type="text"
+            name="reply_to"
+            placeholder="Digite seu email"
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+        </label>
+        <label className="input-label">
+          Deixa aqui um recadinho :D
+          <textarea
+            style={{ resize: "none" }}
+            rows="3"
+            cols="21"
+            className="contact-input"
+            type="text"
+            name="message"
+            placeholder="Pode ser um feedback, elogio, sugestão. Fique à vontade"
+            value={toSend.message}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Enviar</button>
       </form>
-    </>
+    </div>
   );
 }
 
