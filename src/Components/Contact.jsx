b29393608530a0c2bbf60b9fb81819f8b9056f95
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-08 08:22:55 -0300

    Remove linha desncessária

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,6 @@ import { init, send } from "emailjs-com";
 import Loading from "./Nav/Loading";
 import verifyData from "../Helpers/verifyData";
 import contact from "../Images/contact.svg";
-// import Footer from "./Footer";
 init("user_3bguTCeUpXJpsAeCpvezS");
 
 function Contact() {
